c13449b4416be846f4fd2e7393ae991ab9608dc5
Sulaiman YUSOF <syusof@e3r8p1.42.fr>
2016-10-26 14:14:30 +0200

    r

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/03 01:39:59 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/26 13:33:05 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/26 14:13:08 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,18 +65,19 @@ void				ft_read_map(char *file, t_env2 *e);
 int					get_next_line(int const fd, char **line);
 int					key_hook(int keycode, t_env2 *env2);
 int					expose_hook(t_env2 *env2);
-t_coord	get_coord(t_env2 *e2, t_coord c1, t_coord (*f)(int, int, int));
-t_coord		ft_iso(int x, int y, int z);
-int ft_print_line(t_env2 *env2, t_coord coord1, t_coord coord2);
-void draw(t_env2 *env2);
-void		ft_getpoint(char *line, t_env2 *env2, int j);
-void		ft_getpoint2(t_env2 *env2, int j, int k);
-int		ft_get_col(char *line, t_env2 *env2, int i);
-void			ft_read_map(char *file, t_env2 *env2);
-void		ft_read_map2(char *file,t_env2 *env2,char *line);
-void	ft_init(t_env2 *env2);
-void	ft_init2(t_para *para1);
-void		draw2(t_env2 *env2);
-void		key_hook2(int keycode, t_env2 *env2);
+t_coord				get_coord(t_env2 *e2, t_coord c1,
+		t_coord (*f)(int, int, int));
+t_coord				ft_iso(int x, int y, int z);
+int					ft_print_line(t_env2 *env2, t_coord coord1, t_coord coord2);
+void				draw(t_env2 *env2);
+void				ft_getpoint(char *line, t_env2 *env2, int j);
+void				ft_getpoint2(t_env2 *env2, int j, int k);
+int					ft_get_col(char *line, t_env2 *env2, int i);
+void				ft_read_map(char *file, t_env2 *env2);
+void				ft_read_map2(char *file, t_env2 *env2, char *line);
+void				ft_init(t_env2 *env2);
+void				ft_init2(t_para *para1);
+void				draw2(t_env2 *env2);
+void				key_hook2(int keycode, t_env2 *env2);
 
 #endif
