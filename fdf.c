eeb96d68a1382a2b4564d9b04f5684c15b2f4be7
Sulaiman YUSOF <syusof@e3r8p1.42.fr>
2016-10-26 11:56:04 +0200

    r

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/19 10:52:17 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/26 11:16:14 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/26 11:54:29 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int		main(int arc, char **arv)
 	env2->mlx = mlx_init();
 	env2->win = mlx_new_window(env2->mlx, 800, 800, "fdf");
 	if (arc == 2)
-		ft_read_map(arv[1], &env2);
+		ft_read_map(arv[1], env2);
 	mlx_key_hook(env2->win, key_hook, env2);
 	mlx_expose_hook(env2->win, expose_hook, env2);
 	mlx_loop(env2->mlx);
--- ft_read_map.c
+++ ft_read_map.c
@@ -6,13 +6,13 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/03 00:03:17 by syusof            #+#    #+#             */
-/*   Updated: 2016/02/19 04:01:13 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/26 11:55:47 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void		ft_getpoint(char *line, t_env2 ***env2, int j)
+static void		ft_getpoint(char *line, t_env2 *env2, int j)
 {
 	int		i;
 	int		start;
@@ -29,18 +29,20 @@ static void		ft_getpoint(char *line, t_env2 ***env2, int j)
 			while (line[i] && ft_isdigit(line[i]) && line[i] != '+'
 				&& line[i] != '-')
 				i++;
-			((**env2)->coord)[j][k].x = k;
-			((**env2)->coord)[j][k].y = j;
+			((env2)->coord)[j][k].x = k;
+			((env2)->coord)[j][k].y = j;
 			tmp = ft_atoi(ft_strsub(line, start, i - start));
-			((**env2)->coord)[j][k].z = tmp;
+			((env2)->coord)[j][k].z = tmp;
 			k++;
 		}
-		else
+		else if (line[i] == '\t' || line[i] == ' ')
 			i++;
+		else
+			env2->error = 1;
 	}
 }
 
-static	int		ft_get_col(char *line, t_env2 ***env2, int i)
+static	int		ft_get_col(char *line, t_env2 *env2, int i)
 {
 	int k;
 	int n;
@@ -56,16 +58,16 @@ static	int		ft_get_col(char *line, t_env2 ***env2, int i)
 				k++;
 			n++;
 		}
-		else
+		else if(line[k] == '\t' || line[k] == ' ')
 		{
 			k++;
 		}
 	}
-	((**env2)->col)[i] = n;
+	((env2)->col)[i] = n;
 	return (n);
 }
 
-void			ft_read_map(char *file, t_env2 **env2)
+void			ft_read_map(char *file, t_env2 *env2)
 {
 	int		fd;
 	char	*line;
@@ -81,17 +83,17 @@ void			ft_read_map(char *file, t_env2 **env2)
 	while (get_next_line(fd, &line) > 0)
 	{
 		j++;
-		(*env2)->nb_lines = j;
+		(env2)->nb_lines = j;
 	}
 	close(fd);
-	(*env2)->coord = (t_coord**)malloc(sizeof(t_coord*) * j);
+	(env2)->coord = (t_coord**)malloc(sizeof(t_coord*) * j);
 	fd = open(file, O_RDONLY);
-	(*env2)->col = (int*)malloc(sizeof(int) * j);
+	(env2)->col = (int*)malloc(sizeof(int) * j);
 	j = 0;
 	while (get_next_line(fd, &line) > 0)
 	{
-		c = ft_get_col(line, &env2, j);
-		((*env2)->coord)[j] = (t_coord*)malloc(sizeof(t_coord) * c);
+		c = ft_get_col(line, env2, j);
+		((env2)->coord)[j] = (t_coord*)malloc(sizeof(t_coord) * c);
 		j++;
 	}
 	close(fd);
@@ -99,11 +101,11 @@ void			ft_read_map(char *file, t_env2 **env2)
 	fd = open(file, O_RDONLY);
 	while (get_next_line(fd, &line) > 0)
 	{
-		ft_getpoint(line, &env2, j);
+		ft_getpoint(line, env2, j);
 		j++;
 	}
 	close(fd);
-	(*env2)->offx = 0;
-	(*env2)->offy = 0;
-	(*env2)->coeff = 2;
+	(env2)->offx = 0;
+	(env2)->offy = 0;
+	(env2)->coeff = 2;
 }
