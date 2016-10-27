b03719d85c11df33ded3060ba07e8cfb2cc43900
Sulaiman YUSOF <syusof@e3r7p1.42.fr>
2016-10-27 14:37:08 +0200

    r

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/03 01:39:59 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/27 14:32:39 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/27 14:35:28 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,7 @@ void				ft_read_map111(char *file, t_env2 *env2, char *line);
 void				ft_getpoint1(char *line, t_env2 *env2, int j);
 void				ft_getpoint3(t_env2 *env2, int j, int k);
 void				ft_getpoint11(t_env2 *env2, char *line, int j, t_ca *c1);
+void				ft_getpoint111(t_env2 *env2, char *line, int j, t_ca *c1);
 void				ft_getpoint12(t_env2 *env2, char *line, int j, t_ca *c1);
 int					ft_get_col1(char *line, t_env2 *env2, int i);
 void				ft_print_line11(t_env2 *env2, t_para para1);
--- ft_getpoint11.c
+++ ft_getpoint11.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 09:28:35 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/27 14:24:07 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/27 14:36:11 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,31 @@ void		ft_getpoint11(t_env2 *env2, char *line, int j, t_ca *c1)
 	c1->tmp = ft_atoi(s1);
 	ft_free(&s1);
 	((env2)->doub1)[j][c1->k].z = c1->tmp;
+	if (line[c1->i] == ',' && line[c1->i + 1] && line[c1->i + 1] == '0'
+			&& line[c1->i + 2] && line[c1->i + 2] == 'x')
+	{
+		c1->i = c1->i + 3;
+		c1->start = c1->i;
+		while (line[c1->i] && line[c1->i] != '+'
+				&& line[c1->i] != '-' && (ft_isdigit(line[c1->i])
+					|| (line[c1->i] >= 'a' && line[c1->i] <= 'f')
+					|| (line[c1->i] >= 'A' && line[c1->i] <= 'F')))
+			c1->i++;
+		if (c1->start < c1->i)
+		{
+			s1 = ft_strsub(line, c1->start, c1->i - c1->start);
+			c1->tmp = ft_hexaatoi(s1);
+			ft_free(&s1);
+			((env2)->doub1)[j][c1->k].color = c1->tmp;
+		}
+	}
+}
+
+void		ft_getpoint111(t_env2 *env2, char *line, int j, t_ca *c1)
+{
+	char	*s1;
+
+	s1 = NULL;
 	if (line[c1->i] == ',' && line[c1->i + 1] && line[c1->i + 1] == '0'
 			&& line[c1->i + 2] && line[c1->i + 2] == 'x')
 	{
