f61ecaaaa41af95c9407436a5d662fa026935734
Sulaiman YUSOF <syusof@e3r7p1.42.fr>
2016-10-27 10:49:04 +0200

    r

--- ft_draw1.c
+++ ft_draw1.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/26 15:57:03 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/27 10:35:41 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/27 10:41:18 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,8 @@ void		draw11(t_env2 *env2)
 	while (p < env2->nb_lines - 1)
 	{
 		coord1 = get_coord1(env2, env2->doub1[p][env2->col[p] - 1], &ft_iso1);
-		coord2 = get_coord1(env2, env2->doub1[p + 1][env2->col[p] - 1], &ft_iso1);
+		coord2 = get_coord1(env2, env2->doub1[p + 1][env2->col[p] - 1],
+				&ft_iso1);
 		ft_print_line1(env2, coord1, coord2);
 		p++;
 	}
@@ -61,4 +62,3 @@ void		draw11(t_env2 *env2)
 		q++;
 	}
 }
-
--- ft_functiond.c
+++ ft_functiond.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 10:09:21 by syusof            #+#    #+#             */
-/*   Updated: 2016/10/27 10:33:15 by syusof           ###   ########.fr       */
+/*   Updated: 2016/10/27 10:48:13 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,61 +16,59 @@ int			ft_print_line1(t_env2 *env2, t_doub coord1, t_doub coord2)
 {
 	t_para	para1;
 
-	para1.x = coord1.x ;
-	para1.y = coord1.y ;
-	para1.dx = coord2.x -coord1.x  ;
+	para1.x = coord1.x;
+	para1.y = coord1.y;
+	para1.dx = coord2.x - coord1.x;
 	para1.dy = coord2.y - coord1.y;
-	para1.xinc = ( para1.dx > 0 ) ? 1 : -1 ;
-	para1.yinc = ( para1.dy > 0 ) ? 1 : -1 ;
-	para1.dx = abs(para1.dx) ;
-	para1.dy = abs(para1.dy) ;
-	mlx_pixel_put((env2)->mlx, (env2)->win, OX +env2->offx
-			+ para1.x, OY +env2->offy + para1.y, env2->color);
-	if ( para1.dx > para1.dy )
-		ft_print_line11(env2,para1);
+	para1.xinc = (para1.dx > 0) ? 1 : -1;
+	para1.yinc = (para1.dy > 0) ? 1 : -1;
+	para1.dx = abs(para1.dx);
+	para1.dy = abs(para1.dy);
+	mlx_pixel_put((env2)->mlx, (env2)->win, OX + env2->offx
+			+ para1.x, OY + env2->offy + para1.y, env2->color);
+	if (para1.dx > para1.dy)
+		ft_print_line11(env2, para1);
 	else
-		ft_print_line12(env2,para1);
+		ft_print_line12(env2, para1);
 	return (0);
 }
 
 void		ft_print_line11(t_env2 *env2, t_para para1)
 {
-	para1.cumul = para1.dx / 2 ;
-		para1.i = 1;
-		while (  para1.i <= para1.dx )
+	para1.cumul = para1.dx / 2;
+	para1.i = 1;
+	while (para1.i <= para1.dx)
+	{
+		para1.x += para1.xinc;
+		para1.cumul += para1.dy;
+		if (para1.cumul >= para1.dx)
 		{
-			para1.x += para1.xinc ;
-			para1.cumul += para1.dy ;
-			if ( para1.cumul >= para1.dx )
-			{
-				para1.cumul -= para1.dx ;
-				para1.y += para1.yinc ;
-			}
-			mlx_pixel_put((env2)->mlx, (env2)->win, OX +env2->offx
-					+ para1.x, OY +env2->offy + para1.y, env2->color);
-			para1.i++;
+			para1.cumul -= para1.dx;
+			para1.y += para1.yinc;
 		}
-
+		mlx_pixel_put((env2)->mlx, (env2)->win, OX + env2->offx
+				+ para1.x, OY + env2->offy + para1.y, env2->color);
+		para1.i++;
+	}
 }
 
 void		ft_print_line12(t_env2 *env2, t_para para1)
 {
-		para1.cumul = para1.dy / 2 ;
-		para1.i = 1;
-		while (para1.i <= para1.dy)
+	para1.cumul = para1.dy / 2;
+	para1.i = 1;
+	while (para1.i <= para1.dy)
+	{
+		para1.y += para1.yinc;
+		para1.cumul += para1.dx;
+		if (para1.cumul >= para1.dy)
 		{
-			para1.y += para1.yinc ;
-			para1.cumul += para1.dx ;
-			if ( para1.cumul >= para1.dy ) 
-			{
-				para1.cumul -= para1.dy ;
-				para1.x += para1.xinc ; 
-			}
-			mlx_pixel_put((env2)->mlx, (env2)->win, OX +env2->offx
-					+ para1.x, OY +env2->offy + para1.y, env2->color);
-			para1.i++;
+			para1.cumul -= para1.dy;
+			para1.x += para1.xinc;
 		}
-
+		mlx_pixel_put((env2)->mlx, (env2)->win, OX + env2->offx
+				+ para1.x, OY + env2->offy + para1.y, env2->color);
+		para1.i++;
+	}
 }
 
 t_doub		get_coord1(t_env2 *e2, t_doub c1, t_doub (*f)(int, int, int))
@@ -93,5 +91,3 @@ t_doub		ft_iso1(int x, int y, int z)
 	coord1.z = z;
 	return (coord1);
 }
-
-
